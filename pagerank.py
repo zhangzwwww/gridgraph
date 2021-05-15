a6c4b88ba8d0778fc8fe6531cfcdb32fb5556f11
zhangzhaowei <andy_zhangzw@outlook.com>
2021-05-15 22:32:08 +0800

    fix: fix pagerank 0 error

--- pagerank.py
+++ pagerank.py
@@ -1,11 +1,9 @@
 from gridgraph import GridGraph
-import sys
 
 
 if __name__ == "__main__":
     graph = GridGraph("input", 8, 0, 0, 2)
     graph.preprocess()
-
 
     pr = [1 for i in range(0, graph.V)]
     damping = 0.85
@@ -19,26 +17,25 @@ if __name__ == "__main__":
         deg[int(edge["source"])] += 1
         return 1
 
-    graph.streamEdge(compute_deg,vert)
+    graph.streamEdge(compute_deg, vert)
 
-    def contribute(edge,newpr=newpr,pr=pr):
+    def contribute(edge):
         newpr[int(edge["target"])] += pr[int(edge["source"])] / deg[int(edge["source"])]
         return 1
 
-    def compute(vertices, newpr=newpr, pr=pr,d=damping,diff=diff):
+    def compute(vertices, newpr=newpr, pr=pr, d=damping, diff=diff):
         newpr[vertices] = 1 - d + d * newpr[vertices]
         diff += abs(newpr[vertices] - pr[vertices])
         return 1
 
     graph.streamEdge(compute_deg, vert)
 
-    while converged != True :
+    while converged is not True:
         diff = 0
         newpr = [0 for i in range(0, graph.V)]
-        graph.streamEdge(contribute,vert)
+        graph.streamEdge(contribute, vert)
         graph.streamVertice(compute)
-        newpr,pr = pr,newpr
+        newpr, pr = pr, newpr
         converged = (diff/graph.V <= 0.0001)
-        
 
     print("final result of pagerank: ", pr)
